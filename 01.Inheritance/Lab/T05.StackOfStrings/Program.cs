ae01b3c817870c1135e95eb1f72d7a858d424b30
VelizaraSotirova <velizarasotirova@gmail.com>
2023-07-14 09:03:35 +0300

    Solutions for Inheritance Lab

---
+++ 01.Inheritance/Lab/T05.StackOfStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            stackOfStrings.AddRange(new string[] { "1", "2", "3", "4", "5", "6" });
+
+            Console.WriteLine(stackOfStrings.Pop());
+            Console.WriteLine(stackOfStrings.Pop());
+            Console.WriteLine(stackOfStrings.Pop());
+            Console.WriteLine(stackOfStrings.Pop());
+            Console.WriteLine(stackOfStrings.Pop());
+
+        }
+    }
+}
