9ac5ffa6d6971af1b0bfe7bb846727bb81fd381a
VelizaraSotirova <velizarasotirova@gmail.com>
2023-07-16 18:40:26 +0300

    Solutions for Encapsulation Lab

---
+++ 02.Encapsulation/Lab/T04.Team/Team.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        private List<Person> players;
+        public List<Person> Players
+        {
+            get { return this.players; }
+        }
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public string TeamName { get { return name; } }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get { return this.firstTeam.AsReadOnly(); }
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return this.reserveTeam.AsReadOnly(); }
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+    }
+}
