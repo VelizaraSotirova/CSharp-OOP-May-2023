3fab7e665e06119062caa1513f9bd841c8ab53fe
VelizaraSotirova <velizarasotirova@gmail.com>
2023-07-18 18:28:39 +0300

    InterfacesAndAbstractonLab

---
+++ 03.Interfaces and Abstraction/Lab/T02.Cars/Seat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string color, string model)
+        {
+            Color = color;
+            Model = model;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set ; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}";
+        }
+    }
+}
